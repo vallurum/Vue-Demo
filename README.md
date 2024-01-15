7b37bec2f1030448c1716cca3894ddb9b8915eee
Meghana Valluru <meghanav.online@gmail.com>
2024-01-15 15:36:37 +0000

    Added Template to Project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Design911-VueApp
-A Frontend application using Vue and Python.
+# Vite Vue Starter
+
+This is a project template using [Vite](https://vitejs.dev/). It requires [Node.js](https://nodejs.org) v12+.
+
+To start:
+
+```sh
+
+Command to run the backend server
+
+pip install flask
+flask --app main run
+
+command to run the fronte end:
+
+npm install
+npm run dev
+
+# if using yarn:
+yarn
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+  // reactive state
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  // functions that mutate state and trigger updates
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+
+  // lifecycle hooks
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
+
+<template>
+  <div  :style="{ 
+          height: '663.27px',
+          width:'1396.36px',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center'}"
+    >
+    <a-image
+    :width="400"
+    src="https://www.design911.co.uk/uploads/D911-logo-private.png"
+    />
+    <a-button @click="increment">Demo</a-button >
+    <a-button @click="increment">Count is: {{ count }}</a-button >
+
+  </div>
+  
+</template>
