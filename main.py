a52798982e7dec8ebbc6f613563a212958086bdc
Meghana Valluru <meghanav.online@gmail.com>
2024-01-15 21:27:37 +0000

    Added Backend Code to servce csv file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import csv
 
 app = Flask(__name__)
+CORS(app)  
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/getData")
+def get_data():
+    csv_file_path = 'resource/data.csv'
+    data = []
+
+    with open(csv_file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
+
+    # Convert the data to JSON and return
+    return jsonify(data)
